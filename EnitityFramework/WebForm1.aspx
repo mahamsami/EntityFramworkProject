5286bb44ceee6864d74e5624d3781280c52e51c5
apetch nn <apetchnn@gmail.com>
2021-02-15 10:16:22 +0500

    version 1.000 Signed-off-by: apetch nn <apetchnn@gmail.com>

---
+++ EnitityFramework/WebForm1.aspx
@@ -0,0 +1,111 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EnitityFramework.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+
+        <table>
+            <tr>
+                <td>
+                    <asp:Label ID="Label1" runat="server" Text="Id"></asp:Label></td>
+                <td>
+
+
+
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    <br />
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label></td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    <br />
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <asp:Label ID="Label3" runat="server" Text="Salary"></asp:Label></td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                    <br />
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <asp:Label ID="Label4" runat="server" Text="Gender"></asp:Label></td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                    <br />
+                </td>
+            </tr>
+
+            <tr>
+                <td> 
+                    <br />
+                    <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click1" Width="120px" />
+                   
+                </td>
+                <td> 
+                    <br />
+                    <asp:Button ID="Button2" runat="server" Text="Update" Width="120px" OnClick="Button2_Click" />
+                    </td>
+                <td>
+                    <br />
+                    <asp:Button ID="Button3" runat="server" Text="Delete" Width="120px" OnClick="Button3_Click"/>
+                </td>
+                <td>
+                    <br />
+                </td>
+               
+            </tr>
+
+            <tr>
+                <td>
+                    &nbsp;</td>
+            </tr>
+           
+
+        </table>
+
+        <table>
+            <tr>
+                <td>
+                    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                </td>
+                 <td>
+                    <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+                </td>
+                 <td>
+                    <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+
+                <tr>
+                <td>
+                    <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+                </td>
+                 <td>
+                    <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
+                </td>
+                 <td>
+                    <asp:TextBox ID="TextBox10" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ EnitityFramework/WebForm1.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EnitityFramework
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        EmployeeDataEntities3 ede = new EmployeeDataEntities3();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            var data = new EmpData
+            {
+                Id = Convert.ToInt32(TextBox1.Text),
+                Name = TextBox2.Text,
+                Salary = Convert.ToInt32(TextBox3.Text),
+                Gender = TextBox4.Text
+
+            };
+            ede.EmpDatas.Add(data);
+            ede.SaveChanges();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(TextBox1.Text);
+            EmpData x = ede.EmpDatas.Find(id);
+
+            if (x != null)
+            {
+                x.Name = TextBox2.Text;
+                x.Salary = Convert.ToInt32(TextBox3.Text);
+                x.Gender = TextBox4.Text;
+              }
+                ede.SaveChanges();
+            
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(TextBox1.Text);
+
+
+            EmpData x = ede.EmpDatas.Find(id);
+
+            if (x != null)
+            {
+                ede.EmpDatas.Remove(x);
+            }
+
+            ede.SaveChanges();
+        }
+
+        //protected void Button4_Click(object sender, EventArgs e)
+        //{
+        //    //int id = Convert.ToInt32(TextBox1.Text);
+        //    //EmpData x = ede.EmpDatas.Find(id);
+
+        //    //if (x != null)
+        //    //{
+        //    //    TextBox2.Text = x.Name.ToString();
+        //    //    TextBox3.Text = x.Salary.ToString();
+        //    //    TextBox4.Text = x.Gender.ToString();
+        //    //}
+        //    //ede.SaveChanges();
+
+
+        //    SqlConnection con1 = new SqlConnection("Data Source=USER-PC;Initial Catalog=webservice_database;Integrated Security=True");
+        //    DataTable dt = new DataTable();
+        //    con1.Open();
+        //    SqlDataReader myReader = null;
+        //    SqlCommand myCommand = new SqlCommand("select * from customer_registration where username='" + Session["username"] + "'", con1);
+
+        //    myReader = myCommand.ExecuteReader();
+
+        //    while (myReader.Read())
+        //    {
+        //        TextBoxPassword.Text = (myReader["password"].ToString());
+        //        TextBoxRPassword.Text = (myReader["retypepassword"].ToString());
+        //        DropDownListGender.SelectedItem.Text = (myReader["gender"].ToString());
+        //        DropDownListMonth.Text = (myReader["birth"].ToString());
+        //        DropDownListYear.Text = (myReader["birth"].ToString());
+        //        TextBoxAddress.Text = (myReader["address"].ToString());
+        //        TextBoxCity.Text = (myReader["city"].ToString());
+        //        DropDownListCountry.SelectedItem.Text = (myReader["country"].ToString());
+        //        TextBoxPostcode.Text = (myReader["postcode"].ToString());
+        //        TextBoxEmail.Text = (myReader["email"].ToString());
+        //        TextBoxCarno.Text = (myReader["carno"].ToString());
+        //    }
+        //    con1.Close()
+
+
+        //}
+    }
+}
